79037e782ab2bab2dbd67de8370b6b5acb503300
Robert Kirykowicz <rakirykowicz@gmail.com>
2022-03-15 19:34:30 -0500

    Optional Chaining (?.)

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -8,7 +8,7 @@ const flights =
 
 const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
 
-const hours = {
+const openingHours = {
   [weekdays[3]]: {
     open: 12,
     close: 22,
@@ -30,7 +30,7 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-  hours,
+  openingHours,
 
   order(starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
@@ -53,6 +53,20 @@ const restaurant = {
     console.log(otherIngredients);
   },
 };
+
+console.log(restaurant.openingHours.thu);
+//console.log(restaurant.openingHours.mon.open);
+console.log(restaurant.openingHours.mon?.open);
+
+const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+for (const day of days) {
+  const open = restaurant.openingHours[day]?.open ?? 'closed';
+  console.log(`On ${day} we open at ${open}`);
+}
+
+console.log(restaurant.order?.(0, 1));
+console.log(restaurant.orderRisotto?.(0, 1));
 
 /* const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
 
