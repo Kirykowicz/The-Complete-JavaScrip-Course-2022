374fc5940bf1aa5533cee8869c0966d7ed050251
Robert Kirykowicz <rakirykowicz@gmail.com>
2022-03-16 19:21:34 -0500

    Coding Challenge 2

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -53,8 +53,67 @@ const restaurant = {
     console.log(otherIngredients);
   },
 };
+// Challenge #2
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
 
-const properties = Object.keys(openingHours);
+for (const [i, player] of game.scored.entries()) {
+  console.log(`Goal# ${i + 1}: ${player}`);
+}
+
+const odds = Object.values(game.odds);
+let average = 0;
+
+for (const odd of odds) average += odd;
+
+average /= odds.length;
+console.log(average);
+
+const y = Object.entries(game.odds);
+for (const [team, odd] of y) {
+  const teamStr = team === 'x' ? 'draw' : `victory ${game[team]}`;
+  console.log(`Odd of ${teamStr}: ${odd}`);
+}
+// Looping Objects
+/* const properties = Object.keys(openingHours);
 console.log(properties);
 
 let openStr = `We are open on ${properties.length} days:  `;
@@ -72,7 +131,7 @@ console.log(entries);
 
 for (const [key, { open, close }] of entries) {
   console.log(`On ${key}, we are open from ${open} to ${close}`);
-}
+} */
 
 /* console.log(restaurant.openingHours.thu);
 //console.log(restaurant.openingHours.mon.open);
