e7ec7f08343e62ebea0857ed24e725306f937c4a
Robert Kirykowicz <rakirykowicz@gmail.com>
2022-03-17 19:04:29 -0500

    Maps: Fundemental

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -54,9 +54,46 @@ const restaurant = {
   },
 };
 
-// Sets
+// Maps
 
-const ordersSet = new Set([
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenze Italia');
+console.log(rest.set(2, 'Lisbon Portugal'));
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open :D')
+  .set(false, 'We are closed :(');
+console.log(rest);
+
+console.log(rest.get('name'));
+console.log(rest.get(false));
+
+const time = 21;
+
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+console.log(rest.has('categories'));
+rest.delete(2);
+console.log(rest);
+console.log(rest.size);
+//rest.clear();
+
+const arr = [1, 2];
+
+rest.set(arr, 'Test');
+console.log(rest);
+console.log(rest.get(arr));
+
+rest.set(document.querySelector('h1'), 'Heading');
+
+console.log(rest);
+
+// ************* Sets ********************
+
+/* const ordersSet = new Set([
   'Pasta',
   'Pizza',
   'Pizza',
@@ -82,7 +119,7 @@ for (const order of ordersSet) console.log(order);
 const staff = ['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter'];
 
 const staffUnique = [...new Set(staff)];
-console.log(staffUnique);
+console.log(staffUnique); */
 
 // Challenge #2
 /* const game = {
