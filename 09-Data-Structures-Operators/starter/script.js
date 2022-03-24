a85a016d31e99fb3c9b78b6f488ccfd47d10cb6f
Robert Kirykowicz <rakirykowicz@gmail.com>
2022-03-23 19:28:37 -0500

    String Methods Practice

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,8 +1,8 @@
 'use strict';
 
 // Data needed for a later exercise
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+/* const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30'; */
 
 // Data needed for first part of the section
 
@@ -54,7 +54,23 @@ const restaurant = {
   },
 };
 
-// *********** Coding Challenge #4 ************
+// ***** Strings Methods Practice *****
+
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+for (const flight of flights.split('+')) {
+  const [type, from, to, time] = flight.split(';');
+  const output = `${type.startsWith('_Delayed') ? '🔴' : ''}${type.replaceAll(
+    '_',
+    ' '
+  )} from ${from.slice(0, 3).toUpperCase()} to ${to
+    .slice(0, 3)
+    .toUpperCase()} (${time.replace(':', 'h')})`.padStart(44, ' ');
+  console.log(output);
+}
+
+// *********** Coding Challenge #4 ****
 
 /* underscore_case;
 first_name;
@@ -62,7 +78,7 @@ Some_Variable;
 calculate_AGE;
 delayed_departure;
  */
-document.body.append(document.createElement('textarea'));
+/* document.body.append(document.createElement('textarea'));
 document.body.append(document.createElement('button'));
 
 document.querySelector('button').addEventListener('click', function () {
@@ -77,7 +93,7 @@ document.querySelector('button').addEventListener('click', function () {
     )}`;
     console.log(`${output.padEnd(20)}${'✅'.repeat(i + 1)}`);
   }
-});
+}); */
 
 //******* Working with strings part 3**********
 
