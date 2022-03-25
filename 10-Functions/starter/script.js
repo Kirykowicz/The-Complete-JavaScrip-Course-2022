5785a32327b6b3c9d4bbec0d92055444e3fa0ab3
Robert Kirykowicz <rakirykowicz@gmail.com>
2022-03-25 18:55:28 -0500

    Functions Accepting Callback Functions

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,8 +1,28 @@
 'use strict';
 
+// ----Functions Accepting Callback Functions ----
+
+const oneWord = function (str) {
+  return str.replaceAll(' ', '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+  console.log(`Transformed by: the ${fn.name} function`);
+};
+
+transformer('Javascript is the best!', upperFirstWord);
+transformer('Javascript is the best!', oneWord);
+
 // ---- Value vs. Referece ----
 
-const flight = 'LH234';
+/* const flight = 'LH234';
 const jonas = {
   name: 'Jonas Schmedtmann',
   passport: 24739479284,
@@ -29,7 +49,7 @@ const newPassport = function (person) {
 
 console.log(jonas);
 newPassport(jonas);
-console.log(jonas);
+console.log(jonas); */
 
 // ---- Default Parameters ----
 
