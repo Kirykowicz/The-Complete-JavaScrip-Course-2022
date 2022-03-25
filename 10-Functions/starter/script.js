4c31e1cc4ce920547a81bae4f9f23bd4207e0fb8
Robert Kirykowicz <rakirykowicz@gmail.com>
2022-03-24 19:34:54 -0500

    Value vs. Reference

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,8 +1,39 @@
 'use strict';
 
+// ---- Value vs. Referece ----
+
+const flight = 'LH234';
+const jonas = {
+  name: 'Jonas Schmedtmann',
+  passport: 24739479284,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if (passenger.passport === 24739479284) {
+    alert('Check in');
+  } else {
+    alert('Wrong passport');
+  }
+};
+
+checkIn(flight, jonas);
+console.log(flight);
+console.log(jonas);
+
+const newPassport = function (person) {
+  person.passport = Math.random() * 10000000000;
+};
+
+console.log(jonas);
+newPassport(jonas);
+console.log(jonas);
+
 // ---- Default Parameters ----
 
-const bookings = [];
+/* const bookings = [];
 
 const createBooking = function (
   flightNum,
@@ -27,3 +58,4 @@ createBooking('LH123', 2, 800);
 createBooking('LH123', 2);
 createBooking('LH123', 5);
 createBooking('LH123', undefined, 1000);
+ */
