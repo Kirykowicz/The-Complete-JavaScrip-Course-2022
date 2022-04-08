9b6144cc294dbbd7cb71bb2b6814d94b2fe37780
Robert Kirykowicz <rakirykowicz@gmail.com>
2022-04-07 19:01:44 -0500

    Understanding closure, with examples

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,15 +1,67 @@
 'use strict';
 
+// -- More Closure Examples
+
+/* let f;
+
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+g();
+f();
+h();
+f();
+
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+
+boardPassengers(180, 3); */
+
+// -- Closures --
+
+/* const secureBooking = function () {
+  let passengerCount = 0;
+
+  return function () {
+    passengerCount++;
+    console.log(`${passengerCount} passengers`);
+  };
+};
+
+const booker = secureBooking();
+
+booker();
+booker();
+booker(); */
+
 // --- Immediately Invoked Funtion Expressions ---
 
-const runOnce = function () {
+/* const runOnce = function () {
   console.log('this will never run again');
 };
 (function () {
   console.log('this will never run again');
 })();
 
-(() => console.log('this will also never run again'))();
+(() => console.log('this will also never run again'))(); */
 
 // -- Coding Challenge #1 ---
 
