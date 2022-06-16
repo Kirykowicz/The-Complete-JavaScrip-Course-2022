ac6e9e5f828e96117371bacc18f14958bda5809e
Robert Kirykowicz <rakirykowicz@gmail.com>
2022-06-16 17:54:15 -0500

    finishing up section 17

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -1,3 +1,101 @@
 // Importing module
-import './shoppingCart';
+
+// import {
+//   addToCart,
+//   totalPrice as price,
+//   totalQuantity,
+// } from './shoppingCart.js';
+
+// addToCart('bread', 5);
+
+// console.log(price, tq);
+// addToCart('bread', 7);
+
+// console.log(tq);
 console.log('Importing module');
+
+// import * as ShoppingCart from './shoppingCart.js';
+// ShoppingCart.addToCart('bread', 5);
+// console.log(ShoppingCart.totalPrice);
+
+//
+// import add, { addToCart, totalPrice as price, tq } from './shoppingCart.js';
+// console.log(price);
+// console.log(tq);
+//
+
+/* import add, { cart } from './shoppingCart.js';
+add('pizza', 2);
+console.log(cart);
+add('bread', 5);
+add('apples', 4);
+console.log(cart); */
+
+// console.log('start fetching');
+// const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+// const data = await res.json();
+// console.log(data);
+// console.log('Random console.log to test');
+//
+/* 
+const getLastPost = async function () {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await res.json();
+
+  return { title: data.at(-1).title, text: data.at(-1).body };
+};
+const lastPost = getLastPost();
+console.log(lastPost); */
+
+// not a very clean way of doing this. Better to use top level await
+// lastPost.then(last => console.log(last));
+//
+
+/* 
+const lastPost2 = await getLastPost();
+console.log(lastPost2); */
+
+/* const ShoppingCart2 = (function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 237;
+  const totalQuantity = 23;
+
+  const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(`${quantity} ${product} added to cart`);
+  };
+
+  const orderStock = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(`${quantity} ${product} ordered from supplier`);
+  };
+
+  return {
+    addToCart,
+    cart,
+    totalPrice,
+    totalQuantity,
+  };
+})();
+
+ShoppingCart2.addToCart('apple', 4);
+ShoppingCart2.addToCart('pizza', 2);
+ */
+
+import cloneDeep from '../node_modules/lodash-es/cloneDeep.js';
+
+const state = {
+  cart: [
+    { product: 'bread', quantity: 5 },
+    { product: 'pizza', quantity: 5 },
+  ],
+  user: { loggedIn: true },
+};
+
+const stateClone = Object.assign({}, state);
+const stateDeepClone = cloneDeep(state);
+state.user.loggedIn = false;
+console.log(stateClone);
+
+console.log(stateDeepClone);
